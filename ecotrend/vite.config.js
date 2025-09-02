9daa5b2505dc5e30cfc1c6808be4aa13f4798cff
Guilherme de Paula <guilhermekuskowski0@gmail.com>
2025-09-02 09:37:43 -0300

    Update vite.config.js

--- ecotrend/vite.config.js
+++ ecotrend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/EcoTrend/',  // Adicione o nome do seu repositório aqui
 })
+
