d05f1d8cf30968555a169391aa9fabacfe1f602a
Enzo Ferreira <enzocafaria@gmail.com>
2025-09-02 17:43:07 -0300

    estilizando

--- ecotrend/vite.config.js
+++ ecotrend/vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/EcoTrend/',  // Adicione o nome do seu repositório aqui
-})
-
+  base: "/EcoTrend/"
+});
